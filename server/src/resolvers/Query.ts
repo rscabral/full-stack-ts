425eef2b1bace81f3c03f8c66c7b1105be6f9dd0
Rafael Cabral <rafaelsantanacabral@gmail.coma>
2022-06-15 19:57:14 -0300

    refactor: using our db (database) to retrieve data rather than using fixture data

--- server/src/resolvers/Query.ts
+++ server/src/resolvers/Query.ts
@@ -2,32 +2,16 @@ import { TwitterResolverContext } from "../resolvers";
 import { QueryResolvers } from "../resolvers-types.generated";
 
 const queryTwitterResolvers: QueryResolvers<TwitterResolverContext> = {
-  currentUser: () => ({
-    id: "123",
-    name: "Rafael",
-    handle: "Say10",
-    coverUrl: "",
-    avatarUrl: "",
-    createdAt: "",
-    updatedAt: "",
-  }),
-  suggestions: (_, __, { db: _db }) => [
-    {
-      name: "TypeScript Project",
-      handle: "TypeScript",
-      avatarUrl: "http://localhost:3000/static/ts-logo.png",
-      reason: "Because you follow @MichaelLNorth",
-      id: "1",
-    },
-    {
-      name: "jQuery",
-      handle: "jquery",
-      avatarUrl:
-        "http://localhost:3000/static/jquery-logo.jpeg",
-      reason: "Because you follow @FrontendMasters",
-      id: "2",
-    },
-  ],
+  currentUser: (_, __, { db }) => {
+    const [firstUser] = db.getAllUsers();
+
+    if (!firstUser) {
+      throw new Error('current user was requested, but there are no users in the database');
+    }
+
+    return firstUser;
+  },
+  suggestions: (_, __, { db: db }) => db.getAllSuggestions(),
 };
 
 export default queryTwitterResolvers;
